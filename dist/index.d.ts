dc495b638e6d1cfd226d530f9e4668885f2c34f0
Fabio Spampinato <spampinabio@gmail.com>
2018-11-04 18:55:11 +0100

    Ignored dist folder

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { operation } from './types';
-declare function graphqls(...operations: operation[]): (component: any) => any;
-export default graphqls;
--- dist/types.d.ts
+++
@@ -1,6 +0,0 @@
-import { DocumentNode } from 'graphql';
-import { OperationOption } from 'react-apollo/lib/graphql';
-declare type operation = OperationOption & {
-    gql: DocumentNode;
-};
-export { operation };
